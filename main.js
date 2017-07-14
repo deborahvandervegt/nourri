4f1fd04a537673c9cb778568445f1ad1aed37010
Rein van der Vegt <rein.vander.vegt@sogeti.com>
2017-07-14 18:52:20 +0200

    CHA - date formatted to simple date and time NEW - amount is now integer NEW - filtered feedings to today only NEW - added totals (feedings, artificial/breast)

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ var app = angular.module('Nourri', ["firebase"]);
 
 app.controller('nourriController', function($scope, $firebaseArray) {
 
+    $scope.todaysDate = new Date;
+
     var ref = firebase.database().ref().child("amount");
     $scope.amounts = $firebaseArray(ref);
-    
+
+    // Get total amount of feedings equal to input type
+    $scope.getTotal = function(type_input){
+        var total = 0;
+        for(var i = 0; i < $scope.amounts.length; i++){
+            if(type_input == $scope.amounts[i].type){
+                total += $scope.amounts[i].amount;
+            }
+        }
+        return total;
+    }
+
+    $scope.getFeedDate = function(date_string){
+        var d = new Date(date_string);
+        return d;
+    }
+
+    $scope.getOnlyToday = function(item) {
+        var now = new Date;
+        var date = new Date(item.date);
+        return date.setHours(0,0,0,0) == now.setHours(0,0,0,0)
+    }
 });
